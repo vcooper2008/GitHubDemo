f3b0ec7dbc68c19ab78c604c74df0346fb434c34
vcooper <vanessa.cooper1@csuglobal.edu>
2025-04-21 13:43:28 -0500

    Added the main method and it prompts user to input a number.

--- src/FactorialCalculator.java
+++ src/FactorialCalculator.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class FactorialCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a non-negative integer: ");
+        int n = scanner.nextInt();
+    }
 }
