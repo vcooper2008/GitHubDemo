98dfe5554c354e308e86196428c8f29f57835a4d
vcooper <vanessa.cooper1@csuglobal.edu>
2025-04-21 13:47:01 -0500

    Added code to check for positive numbers and call the calculateFactorial function. That function is not yet implemented.

--- src/FactorialCalculator.java
+++ src/FactorialCalculator.java
@@ -6,5 +6,14 @@ public class FactorialCalculator {
 
         System.out.print("Enter a non-negative integer: ");
         int n = scanner.nextInt();
+
+        if(n < 0) {
+            System.out.println("Factorial is not defined for negative numbers.");
+        } else {
+            long factorial = calculateFactorial(n);
+            System.out.println("The factorial of " + n + " is: " + factorial);
+        }
+
+        scanner.close();
     }
 }
