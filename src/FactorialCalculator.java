0c6014853a304bd0822a97c9cd969d8475e9a443
vcooper <vanessa.cooper1@csuglobal.edu>
2025-04-21 13:52:58 -0500

    Added calculateFactorial function. Code now complete.

--- src/FactorialCalculator.java
+++ src/FactorialCalculator.java
@@ -16,4 +16,16 @@ public class FactorialCalculator {
 
         scanner.close();
     }
+
+    public static long calculateFactorial(int num) {
+        if (num == 0) {
+            return 1;
+        } else {
+            long result = 1;
+            for(int i = 1; i <= num; i++) {
+                result *= i;
+            }
+            return result;
+        }
+    }
 }
